cf22f04b884d57f87133488bd376767d019de65e
Jingyi Wang <jingyiwang@Jingyis-MacBook-Pro.local>
2017-04-09 12:41:33 -0700

    Exp2

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,17 +1,17 @@
 // Measuring the Critical Rendering Path with Navigation Timing
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
-function logCRP() {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
-  var stats = document.getElementById("crp-stats");
-  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
-}
+// function logCRP() {
+//   var t = window.performance.timing,
+//     dcl = t.domContentLoadedEventStart - t.domLoading,
+//     complete = t.domComplete - t.domLoading;
+//   var stats = document.getElementById("crp-stats");
+//   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+// }
 
-window.addEventListener("load", function(event) {
-  logCRP();
-});
+// window.addEventListener("load", function(event) {
+//   logCRP();
+// });
 
 var stateObj = { foo: "bar" };
 history.pushState(stateObj, "page project-2048", "project-2048.html");
